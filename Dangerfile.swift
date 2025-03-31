3d1cce7391ccff10eba47f201b1dda41a5661145
Alin Popa <apopa@eightpoint.io>
2025-03-31 17:30:19 +0300

    Update Dangerfile.swift to use GitHub

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -2,7 +2,7 @@ import Danger
 
 let danger = Danger()
 let git = danger.git
-let gitLab = danger.gitLab
+let github = danger.github
 
 let totalChangedFiles = git.createdFiles.count + git.modifiedFiles.count - git.deletedFiles.count
 
@@ -17,7 +17,7 @@ if totalChangedFiles > 40 {
 
 // MARK: Dangerfile changed
 if git.modifiedFiles.contains("Dangerfile") {
-    warn("⚠️ This MR modified Dangerfile! Check the rules!")
+    warn("⚠️ This PR modified Dangerfile! Check the rules!")
 }
 
 // MARK: Too Many Commits
@@ -25,47 +25,44 @@ if git.commits.count >= 10 {
     warn("⚠️ Please keep pull requests to at most 10 commits.")
 }
 
-// MARK: Total lines of code
-let allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
-if !allSourceFiles.isEmpty {
-    let totalLinesOfCode = allSourceFiles.reduce(0) { partialResult, file in
-
-        if let diff = try?  danger.utils.diff(forFile: file, sourceBranch: danger.gitLab.mergeRequest.targetBranch).get() {
-            let changes = diff.changes
-            switch changes {
-            case .created(let addedLines):
-                return partialResult + addedLines.count
-            case .deleted(let deletedLines):
-                return partialResult + deletedLines.count
-            default:
-                return partialResult + 0
-            }
-        } else {
-            return partialResult + 0
-        }
-    }
-
-    if totalLinesOfCode > 600 {
-        fail("🚨 Files should contain less than 600 lines of code.")
-
-    } else if totalLinesOfCode > 400 {
-        warn("⚠️ Consider keeping changes to less than 400 lines of code.")
-    }
-}
-
-
-if let gitLab {
-    // MARK: Merge Request Description
-    if (gitLab.mergeRequest.description.count < 10) {
-        fail("🚨 Please provide a summary in the Merge Request description to help your colleagues understand the MR purpose.")
+if let github {
+    // MARK: Pull Request Description
+    if (github.pullRequest.description.count < 10) {
+        fail("🚨 Please provide a summary in the Pull Request description to help your colleagues understand the MR purpose.")
     }
 
 
     // MARK: Assignee needed
-    if gitLab.mergeRequest.assignee == nil {
-        warn("⚠️ Please assign someone to this MR.")
+    if github.pullRequest.assignee == nil {
+        warn("⚠️ Please assign someone to this PR.")
     }
 
+    // MARK: Total lines of code
+    let allSourceFiles = git.modifiedFiles + git.createdFiles
+    if !allSourceFiles.isEmpty, let github {
+        let totalLinesOfCode = allSourceFiles.reduce(0) { partialResult, file in
+            if let diff = try?  danger.utils.diff(forFile: file, sourceBranch: github.pullRequest.targetBranch).get() {
+                let changes = diff.changes
+                switch changes {
+                case .created(let addedLines):
+                    return partialResult + addedLines.count
+                case .deleted(let deletedLines):
+                    return partialResult + deletedLines.count
+                default:
+                    return partialResult + 0
+                }
+            } else {
+                return partialResult + 0
+            }
+        }
+
+        if totalLinesOfCode > 600 {
+            fail("🚨 Files should contain less than 600 lines of code.")
+
+        } else if totalLinesOfCode > 400 {
+            warn("⚠️ Consider keeping changes to less than 400 lines of code.")
+        }
+    }
 }
 
 // MARK: Swiftlint
